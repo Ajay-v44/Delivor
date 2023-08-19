777b1cadf7f12ec0e2835def6c62434d3a388c74
Ajay v <vajay4834@gmail.com>
2023-08-19 18:41:48 +0530

    added link to menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Image from 'next/image'
+import Link from 'next/link';
 import React, { useState } from 'react'
 
 const links=[
     {id:1,title:"Homepage",url:"/"},
-    {id:1,title:"Homepage",url:"/"},
-    {id:1,title:"Homepage",url:"/"},
-]
+    {id:2,title:"Menu",url:"/menu"},
+    {id:3,title:"Working Hours",url:"/"},
+    {id:4,title:"Contact",url:"/"},
+];
 const Menu = () => {
     const [open, setOpen] = useState(false)
     return (
@@ -17,7 +19,9 @@ const Menu = () => {
                 <Image src="/close.png" alt="" width={20} height={20} onClick={() => setOpen(false)} />
             )}
             <div>
-
+                {links.map(items=>(
+                    <Link href={'item.url'} key={items.id}>{items.title}</Link>
+                ))}
             </div>
         </div>
     )
