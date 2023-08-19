3e17e316375800f4c123fd047b29482549d107f6
Ajay v <vajay4834@gmail.com>
2023-08-19 12:07:58 +0530

    added usestate to menu

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div className='h-12 text-red-500 p-4 flex  items-center justify-between border-b-2 border-b-red-500 uppercase'>
       {/* LOGO */}
-    <div>
+    <div className='text-xl'>
       <Link href="/">Delivor</Link>  
     </div>
     {/*MOBILE MENU */}
