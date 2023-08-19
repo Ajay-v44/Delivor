8d0a1dd13d172378ae01cb4bf7faa60b354c5d9d
Ajay v <vajay4834@gmail.com>
2023-08-19 12:43:59 +0530

    changed menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -2,18 +2,25 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 
+const links=[
+    {id:1,title:"Homepage",url:"/"},
+    {id:1,title:"Homepage",url:"/"},
+    {id:1,title:"Homepage",url:"/"},
+]
 const Menu = () => {
     const [open, setOpen] = useState(false)
-  return (
-    <div>
-       {!open?(
-        <Image src="/open.png" alt="" width={20} height={20} onClick={()=>setOpen(true)}/>
-       ):(
-        <Image src="/close.png" alt="" width={20} height={20}onClick={()=>setOpen(false)}/>
-       )}
-        
-    </div>
-  )
+    return (
+        <div>
+            {!open ? (
+                <Image src="/open.png" alt="" width={20} height={20} onClick={() => setOpen(true)} />
+            ) : (
+                <Image src="/close.png" alt="" width={20} height={20} onClick={() => setOpen(false)} />
+            )}
+            <div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Menu
