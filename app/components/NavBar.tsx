d5b082d47250fa3240d72b13dafe297b3a5e07bb
Ajay v <vajay4834@gmail.com>
2023-08-20 16:27:41 +0530

    nav bar updated

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 import Menu from './Menu'
+import CartIcon from './CartIcon'
+import Image from 'next/image';
 
 const NavBar = () => {
+  const user = false
   return (
     <div className='h-12 text-red-500 p-4 flex  items-center justify-between border-b-2 border-b-red-500 uppercase'>
       <div className='hidden md:flex gap-4 '>
@@ -17,9 +20,20 @@ const NavBar = () => {
       {/*MOBILE MENU */}
       <div className='md:hidden'>
         <Menu />
+        </div>
+        {/* Right links */}
+        <div className='hidden md:flex gap-4  item-center '>
+          <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+            <Image src="/phone.png" alt="" width={20} height={20} />
+            <span>12 456 78</span>
+          </div>
+          {!user ? (
+            <Link href='/login'>Login</Link>
+          ) : (<Link href='/Orders'>orders</Link>)}
+          <CartIcon />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar
