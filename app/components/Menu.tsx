a4081c6af348ef5ddccf500e559278ee4e8b14cb
Ajay v <vajay4834@gmail.com>
2023-08-20 11:04:08 +0530

    medified menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link';
 import React, { useState } from 'react'
 import CartIcon from './CartIcon';
 
-const links=[
-    {id:1,title:"Homepage",url:"/"},
-    {id:2,title:"Menu",url:"/menu"},
-    {id:3,title:"Working Hours",url:"/"},
-    {id:4,title:"Contact",url:"/"},
+const links = [
+    { id: 1, title: "Homepage", url: "/" },
+    { id: 2, title: "Menu", url: "/menu" },
+    { id: 3, title: "Working Hours", url: "/" },
+    { id: 4, title: "Contact", url: "/" },
 ];
 const Menu = () => {
     const [open, setOpen] = useState(false);
-    const user=false
+    const user = false
     return (
         <div>
             {!open ? (
@@ -20,14 +20,16 @@ const Menu = () => {
             ) : (
                 <Image src="/close.png" alt="" width={20} height={20} onClick={() => setOpen(false)} />
             )}
-            <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
-                {links.map(items=>(
-                    <Link href={'item.url'} key={items.id}>{items.title}</Link>
-                ))}
-               {!user? <Link href="/login">Login</Link>:
-               <Link href="/order">Orders</Link>}
-               <Link href="/cart"><CartIcon/></Link>
-            </div>
+           {open && (
+             <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
+             {links.map(items => (
+                 <Link href={'item.url'} key={items.id} onClick={() => setOpen(false)}>{items.title}</Link>
+             ))}
+             {!user ? <Link href="/login" onClick={()=>setOpen(false)}>Login</Link> :
+                 <Link href="/order" onClick={()=>setOpen(false)}>Orders</Link>}
+             <Link href="/cart" onClick={()=>setOpen(false)} ><CartIcon /></Link>
+         </div>
+           )}
         </div>
     )
 }
