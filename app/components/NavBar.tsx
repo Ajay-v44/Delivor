5f3b1d15c75a5501a67cfb119f215cf634928846
Ajay v <vajay4834@gmail.com>
2023-08-20 12:42:51 +0530

    added link for mobile devices

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,14 +5,19 @@ import Menu from './Menu'
 const NavBar = () => {
   return (
     <div className='h-12 text-red-500 p-4 flex  items-center justify-between border-b-2 border-b-red-500 uppercase'>
+      <div className='hidden md:flex gap-4 '>
+        <Link href='/'>HomePage</Link>
+        <Link href='/menu'>Menu</Link>
+        <Link href='/'>Contact</Link>
+      </div>
       {/* LOGO */}
-    <div className='text-xl'>
-      <Link href="/">Delivor</Link>  
-    </div>
-    {/*MOBILE MENU */}
-    <div>
-      <Menu/>
-    </div>
+      <div className='text-xl'>
+        <Link href="/">Delivor</Link>
+      </div>
+      {/*MOBILE MENU */}
+      <div className='md:hidden'>
+        <Menu />
+      </div>
     </div>
   )
 }
