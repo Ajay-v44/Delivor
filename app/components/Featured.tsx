c8b1ce22192705709a8181eae6a5549e4c72dae6
Ajay v <vajay4834@gmail.com>
2023-08-21 18:36:21 +0530

    updated features

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -1,8 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className='w-screen overflow-x-scroll text-red-500'>
+      {/* WRAPPER */}
+      <div className='w-max flex'>
+        {/* SINGLE ITEM */}
+
+        <div className=''>
+          {/* image container  */}
+          <div className='relative'>
+            <Image src="" alt="" fill />
+
+          </div>
+
+          {/* text container */}
+          <div  className=''>
+            <h1>Title</h1>
+            <p>Desc</p>
+            <span>123</span>
+            <button>Add to cart</button>
+
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
