72f6031c37364b4f19e93420aef67fb6f8d7c2f4
Ajay v <vajay4834@gmail.com>
2023-08-21 18:23:10 +0530

    slider completed

--- app/components/slider.tsx
+++ app/components/slider.tsx
@@ -32,13 +32,13 @@ const slider = () => {
     }, [])
 
     return (
-        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
-            <div className='h-1/2 flex items-center flex-col gap-0 text-red-500 font-bold'>
-                <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>{data[currentSlide].title}</h1>
+        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
+            <div className='flex-1 flex items-center flex-col gap-0 text-red-500 font-bold '>
+                <h1 className='text-5xl text-center uppercase p-4 md:text-6xl md:p-10 xl:text-7xl'>{data[currentSlide].title}</h1>
                 <button className='bg-red-500 text-white py-4 px-8'>Order</button>
             </div>
 
-            <div className='w-full h-1/2 relative'>
+            <div className='w-full flex-1 relative '>
                 <Image src={data[currentSlide].image} alt="" fill className='object-cover' />
 
             </div>
