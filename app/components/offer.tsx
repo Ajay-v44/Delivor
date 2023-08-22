200044530819fb058358977c2915c64154e62e6b
Ajay v <vajay4834@gmail.com>
2023-08-22 18:38:21 +0530

    completed counter

--- app/components/offer.tsx
+++ app/components/offer.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import CountDown from './CountDown'
 
 const offer = () => {
   return (
@@ -8,6 +9,7 @@ const offer = () => {
       <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6 '>
         <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & French Fry </h1>
         <p className='text-white xl:text-xl'>The best Food in town.Enjoy the world of mouth watering delicious food.</p>
+        <CountDown/>
         <button className='bg-red-500 text-white rounded-md py-3 px-6'>Order Now</button>
       </div>
       {/** image-container */}
