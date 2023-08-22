fa417a0a5d2b445d858198836d13f6fd41e1d8c3
Ajay v <vajay4834@gmail.com>
2023-08-22 18:28:01 +0530

    created offer section

--- app/components/offer.tsx
+++ app/components/offer.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const offer = () => {
   return (
-    <div>offer</div>
+    <div className='bg-black h-screen flex flex-col md:flex-row'>
+      {/** text-container */}
+      <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6 '>
+        <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & French Fry </h1>
+        <p className='text-white xl:text-xl'>The best Food in town.Enjoy the world of mouth watering delicious food.</p>
+        <button className='bg-red-500 text-white rounded-md py-3 px-6'>Order Now</button>
+      </div>
+      {/** image-container */}
+      <div className=' flex-1 w-full relative'>
+        <Image src="/offerProduct.png" alt="" fill className='object-contain' />
+      </div>
+    </div>
   )
 }
 
