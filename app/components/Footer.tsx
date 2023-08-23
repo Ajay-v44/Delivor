fa1d991e2ced9e36eaf1d97184925135e174f09d
Ajay v <vajay4834@gmail.com>
2023-08-23 18:05:51 +0530

    footer modified

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='h-12 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
-      <Link href="/" className='font-bold text-xl'>Delivor</Link>
-      <p>©️ ALL RIGHTS RESERVED</p>
+    <div className='py-8 lg:py-16 xl:py-24 bg-red-500'>
+      <div className='container mx-auto px-6 lg:px-20 xl:px-40 flex flex-col lg:flex-row items-center justify-between'>
+        <div className='text-center lg:text-left mb-6 lg:mb-0'>
+          <a className='font-bold text-xl text-white transition-transform transform hover:scale-105'>
+            Delivor
+          </a>
+          <p className='text-xs mt-1 text-gray-300 animate-pulse'>©️ ALL RIGHTS RESERVED</p>
+        </div>
+        <div className='text-center lg:text-right'>
+          <p className='text-sm text-gray-300'>Made with</p>
+          <p className='font-bold text-lg text-white'>❤️</p>
+          <p className='text-sm'>by Ajay</p>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -5,13 +5,20 @@ import { menu } from '../components/data'
 const MenuPage = () => {
   return (
     <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
-      MenuPage
+
       {menu.map(category => (
-        <Link href={category.slug} key={category.id}>
-          <div className=''>
-            <h1>{category.title}</h1>
-            <p>{category.desc}</p>
-            <button className=''>Explore</button>
+        <Link href={`/menu/${category.slug}`} key={category.id} className='w-full h-1/3 bg-cover p-8 md:h-1/2' style={{ backgroundImage: `url(${category.img})` }}>
+
+          <div className={`text-(${category.color})`}>
+            <h1 className='upper-case font-bold text-3xl'>{category.title}</h1>
+            <p className='text-sm my-8'>{category.desc}</p>
+            <button className='text-white bg-gradient-to-r from-red-500 to-yellow-500 hover:from-red-600 hover:to-yellow-600 py-2 px-6 rounded-md shadow-md transition-colors duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg'>
+  Explore
+</button>
+
+
+
+
 
           </div>
 
