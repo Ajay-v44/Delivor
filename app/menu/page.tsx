07c7875cf6024f1f2cd38ecf71d7c00238f4f45d
Ajay v <vajay4834@gmail.com>
2023-08-23 17:04:27 +0530

    created menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
+import { menu } from '../components/data'
 
 const MenuPage = () => {
   return (
-    <div>MenuPage</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+      MenuPage
+      {menu.map(category => (
+        <Link href={category.slug} key={category.id}>
+          <div className=''>
+            <h1>{category.title}</h1>
+            <p>{category.desc}</p>
+            <button className=''>Explore</button>
+
+          </div>
+
+        </Link>
+      ))}
+    </div>
   )
 }
 
