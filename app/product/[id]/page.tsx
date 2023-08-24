8dcee27b3e47a53eae1fe51bca4ed71519b67e63
Ajay v <vajay4834@gmail.com>
2023-08-24 17:52:01 +0530

    created single product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,9 +1,23 @@
+import { singleProduct } from '@/app/components/data'
+import Image from 'next/image'
 import React from 'react'
 
 const SingleProductPage = () => {
     return (
-        <div>
-            SingleProductPage
+        <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-red-500 md:flex-row'>
+            {/* Image Container */}
+            {singleProduct.img && (
+                <div className=''>
+                    <Image src={singleProduct.img} alt="" className='object-contain' fill />
+
+
+                </div>
+            )}
+            {/*Text-Container */}
+            <div className=''>
+                <h1>{singleProduct.title}</h1>
+                <p>{singleProduct.desc}</p>
+            </div>
         </div>
     )
 }
