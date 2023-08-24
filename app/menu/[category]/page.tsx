7aac44bc5b6c3753ebc598cd02b8d9c500e708fb
Ajay v <vajay4834@gmail.com>
2023-08-24 12:08:28 +0530

    category page styled and completed

--- app/menu/[category]/page.tsx
+++ app/menu/[category]/page.tsx
@@ -5,16 +5,22 @@ import React from 'react'
 
 const categorypage = () => {
   return (
-    <div className='flex text-red-500'>
-      {pizzas.map(item=>(
-        <Link className='w-full h-[60vh] border-r-2 border-red-500 sm:w-1/2 lg:w-1/3'  href={`/product/${item.id}`} key={item.id}>
+    <div className='flex flex-wrap text-red-500'>
+      {pizzas.map(item => (
+        <Link className='w-full h-[60vh] border-r-2  border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-100' href={`/product/${item.id}`} key={item.id}>
           {/* Image Container */}
-          {item.img &&(
-            <div className='relative'>
-              <Image src={item.img} alt=""/>
+          {item.img && (
+            <div className='relative h-[80%]'>
+              <Image src={item.img} alt="" fill className='object-contain' />
 
             </div>
           )}
+          {/* Text Container */}
+          <div>
+            <h1 className='flex items-center justify-between font-bold '>{item.title}</h1>
+            <h2 className='text-xl'>${item.price}</h2>
+            <button className='hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md'>Add to cart</button>
+          </div>
         </Link>
       ))}
     </div>
