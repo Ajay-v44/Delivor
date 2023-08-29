c5e1ecd4e0f0967f3ea2ddcf39e388258f986b3f
Ajay v <vajay4834@gmail.com>
2023-08-29 16:35:24 +0530

    made price changing dynamically according to quamtity

--- app/components/PriceComponent.tsx
+++ app/components/PriceComponent.tsx
@@ -1,39 +1,50 @@
 "use client";
 import React, { useEffect, useState } from 'react'
-type props={
-    price:number;
-    id:number;
+type props = {
+    price: number;
+    id: number;
     options?: { title: string; additionalPrice: number }[];
 }
 
-const PriceComponent = ({price,id,options}:props ) => {
+const PriceComponent = ({ price, id, options }: props) => {
     const [total, settotal] = useState(price);
     const [quantity, setquantity] = useState(1);
     const [selected, setselected] = useState(0);
-  return (
-    <div className="flex flex-col gap-4">
-    
-        <h2 className='text-2xl font-bold'>${price.toFixed(2)}</h2>
-       { /* options container */}
-       <div className='flex gap-4'>
-        {options?.map(option=>(
-            <button key={option.title} className='p-2 ring-1 ring-red-400 rounded-md '>{option.title}</button>
-        ))}
-       </div>
-{/* Quantity and add button container */}
-       <div className='flex justify-between items-center'>
-        <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
-            <span>Quantity</span>
-            <div className='flex gap-4 items-center'>
-                <button>{'<'}</button>
-                <span>1</span>
-                <button>{'>'}</button>
+
+    useEffect(()=>{
+        settotal(quantity=(options?price+options[selected].additionalPrice:price))
+
+    },[quantity,selected])
+    return (
+        <div className="flex flex-col gap-4">
+
+            <h2 className='text-2xl font-bold'>${total.toFixed(2)}</h2>
+            { /* options container */}
+            <div className='flex gap-4'>
+                {options?.map((option, index) => (
+                    <button key={option.title} className='min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md ' style={{
+                        background: selected === index ? "rgb(248 113 113)" : "white",
+                        color: selected === index ? "white" : "red",
+
+                    }}
+                        onClick={() => setselected(index)}
+                    >{option.title}</button>
+                ))}
+            </div>
+            {/* Quantity and add button container */}
+            <div className='flex justify-between items-center'>
+                <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
+                    <span>Quantity</span>
+                    <div className='flex gap-4 items-center'>
+                        <button onClick={() => setquantity(prev => (prev > 1 ? prev - 1 : 1))}>{'<'}</button>
+                        <span>{quantity}</span>
+                        <button onClick={() => setquantity(prev => (prev < 9 ? prev + 1 : 9))}>{'>'}</button>
+                    </div>
+                </div>
+                <button className='uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500'>Add to Cart</button>
             </div>
         </div>
-                <button className='uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500'>Add to Cart</button>
-       </div>
-    </div>
-  ) 
+    )
 }
 
 export default PriceComponent
