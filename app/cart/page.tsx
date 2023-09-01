bdf6366f110ae1a04b5e951b5bb638fa727620f7
Ajay v <vajay4834@gmail.com>
2023-09-01 10:14:09 +0530

    created cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>
-      
+    <div className=''>
+      {/* product container */}
+      <div className=''>
+        {/* single item container */}
+        <div className=''>
+          <h1>p name</h1>
+          <span>size</span>
+
+
+        </div>
+        <h2>price</h2>
+        <span>x</span>
+
+      </div>
+      {/* payment container */}
+      <div className=''>
+
+      </div>
+
+
     </div>
   )
 }
