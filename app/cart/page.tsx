10cb3cacc12ee137f688f490a8053ee86fa947d3
Ajay v <vajay4834@gmail.com>
2023-09-01 10:31:36 +0530

    styled cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,23 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div className=''>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 overflow-scroll'>
       {/* product container */}
-      <div className=''>
+      <div className='h-1/2 p-4 flex flex-col justify-center'>
         {/* single item container */}
-        <div className=''>
-          <h1>p name</h1>
+        <div className='flex items-center justify-between mb-4 overflow-scroll'>
+          <Image src="/temporary/p1.png" alt="" width={100} height={100}/>
+          <h1 className='uppercase text-xl font-bold'>p name</h1>
           <span>size</span>
 
 
         </div>
-        <h2>price</h2>
-        <span>x</span>
+        <h2 className='font-bold'>price</h2>
+        <span className='cursor-pointer'>x</span>
 
       </div>
       {/* payment container */}
+      <div className='h-1/2 p-4 bg-fuchsia-50 '>
       <div className=''>
+        <span className=''>subtotal</span>
+        <span className=''>181.70</span>
+        
+
+      </div>
+      <hr className=''/>
+      <button className='bg-red-500 text-white p-3 round w={100} height={100}'>Checkout</button>
+  
 
       </div>
 
