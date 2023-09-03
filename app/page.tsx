8bd8db4e3919430a7fa2b1b08e8f7c2ce68d5006
Ajay v <vajay4834@gmail.com>
2023-09-03 17:16:09 +0530

    changes made

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 
 import Featured from './components/Featured'
-import Slider from './components/Slider'
 import Offer from './components/Offer'
+import Slider from './components/Slider'
+
 export default function Home() {
   return (
     <main>
