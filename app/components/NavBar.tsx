ad07f2ea39281d674948a277569ac7298e71f43f
Ajay v <vajay4834@gmail.com>
2023-09-03 18:26:58 +0530

    error solved

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -20,17 +20,17 @@ const NavBar = () => {
       {/*MOBILE MENU */}
       <div className='md:hidden'>
         <Menu />
+      </div>
+      {/* Right links */}
+      <div className='hidden md:flex gap-4  item-center justify-end flex-1'>
+        <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>12 456 78</span>
         </div>
-        {/* Right links */}
-        <div className='hidden md:flex gap-4  item-center justify-end flex-1'>
-          <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
-            <Image src="/phone.png" alt="" width={20} height={20} />
-            <span>12 456 78</span>
-          </div>
-          {!user ? (
-            <Link href='/login'>Login</Link>
-          ) : (<Link href='/Orders'>orders</Link>)}
-          <CartIcon />
+        {!user ? (
+          <Link href='/login'>Login</Link>
+        ) : (<Link href='/Orders'>orders</Link>)}
+        <CartIcon />
       </div>
     </div>
   );
